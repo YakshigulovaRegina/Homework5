80c8f552e848ca8a300bf0f7c62f09fb5ed7f360
Regina <gumerovars@gmail.com>
2023-01-15 19:13:10 +0500

    first task

---
+++ ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = generateArray(7);  
+search(array);
+PrintArray(array);
+Console.WriteLine("Четных чисел : " + (search(array)));
+
+int[] generateArray (int arraySize)
+{
+    int[] array = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        array[i] = new Random().Next(100,999);
+    }  
+    return array;
+}
+
+int search (int[] array)
+{ 
+    int number = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] %2 == 0)
+    {
+        number = number + 1;
+    } 
+}
+return number;
+}
+
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
